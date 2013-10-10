a47903b68899bdd3d11234c147af39c625305fb5
Paulovich Anton <p.a.n.tosha@gmail.com>
2013-10-10 15:52:01 +0300

    Add labs 1-4
    Write on MASM

---
+++ Lab3.asm
@@ -0,0 +1,204 @@
+    .486                       ; create 32 bit code
+    .model flat, stdcall       ; 32 bit memory model
+    option casemap :none       ; case sensitive
+ 
+    __UNICODE__ equ 1
+    
+    E_RANGE      equ  1
+    E_OPNOTSUPP  equ  2
+    E_SYNTAX     equ  3
+ 
+    include windows.inc
+    include masm32.inc
+    include kernel32.inc
+    include shell32.inc
+    include msvcrt.inc
+    include \masm32\macros\macros.asm
+
+    includelib masm32.lib
+    includelib kernel32.lib
+    includelib shell32.lib
+    includelib msvcrt.lib
+
+    apply_operation PROTO :DWORD, :DWORD
+    print_help PROTO
+    print_error PROTO
+    
+; ariphmetic operations
+
+    addition PROTO :DWORD, :DWORD
+    subtraction PROTO :DWORD, :DWORD
+    multiply PROTO :DWORD, :DWORD
+    division PROTO :DWORD, :DWORD
+
+    .data
+    errno       dd   0
+    operation   dd   0
+    
+    .code
+
+start:
+ 
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+
+main proc
+
+    LOCAL args: LPWSTR
+    LOCAL nArgs: DWORD
+    LOCAL ArgList: DWORD
+    LOCAL x: REAL8
+    LOCAL y: REAL8
+    
+    invoke GetCommandLineW
+    mov args, EAX
+    invoke CommandLineToArgvW, args, addr nArgs
+    mov ArgList, EAX
+    .IF ArgList == 0
+        print "Error: can't get command line arguments."
+        jmp close
+    .ENDIF
+    cmp nArgs, 4
+    jne command_line_error 
+    
+    mov ESI, ArgList
+
+    add ESI, 4
+    invoke crt_swscanf, dword ptr [ESI], cfm$("%8d"), ADDR x
+    cmp EAX, 1    
+    jne command_line_error
+    
+    add ESI, 4
+    invoke crt_swscanf, dword ptr [ESI], cfm$("%c"), ADDR operation
+    cmp EAX, 1
+    jne command_line_error
+    
+    add ESI, 4
+    invoke crt_swscanf, dword ptr [ESI], cfm$("%8d"), ADDR y
+    cmp EAX, 1
+    jne command_line_error
+    
+    invoke apply_operation, x, y
+
+    .IF errno != 0
+        invoke print_error
+    .ELSEIF
+        printf("%d %c %d = %d", x, operation, y, EAX)
+    .ENDIF
+    
+    jmp close
+    
+  command_line_error:
+    mov errno, E_SYNTAX
+    invoke print_error
+  close:
+; free memory
+    invoke LocalFree, ArgList
+    invoke ExitProcess,0
+    
+    ret
+
+main endp
+
+
+print_help proc
+
+    print "AVS Labs, 2013"
+    print chr$(13,10,13,10)
+    print "SYNTAX: avs-labs [first number] [operation] [second number]"
+    print chr$(13,10)
+    print "        first number : first number"
+    print chr$(13,10)
+    print "        operation    : operation that be applied to expression"
+    print chr$(13,10)
+    print "        second number: another number."
+    print chr$(13,10,13,10)
+    print "        Output is to STDOUT. It can redirected to a file."
+    print chr$(13,10)
+	ret
+	
+print_help endp
+
+
+print_error proc
+
+    .IF errno == E_RANGE
+        print "Error: out of range."
+    .ELSEIF errno == E_OPNOTSUPP
+        print "Error: operation not supported."
+    .ELSEIF errno == E_SYNTAX
+        invoke print_help
+    .ENDIF
+	ret
+	
+print_error endp
+
+apply_operation proc x:DWORD, y:DWORD 
+    
+    .IF operation == "+"
+        invoke addition, x, y
+    .ELSEIF operation == "-"
+        invoke subtraction, x, y
+    .ELSEIF operation == "*"
+        invoke multiply, x, y
+    .ELSEIF operation == "/"
+        .IF y == 0
+            mov errno, E_RANGE 
+        .ELSEIF
+            invoke division, x, y
+        .ENDIF
+    .ELSEIF
+        mov errno, E_OPNOTSUPP
+    .ENDIF
+	ret
+	
+apply_operation endp
+
+addition proc x:DWORD,y:DWORD
+    
+    mov EAX, x
+    add EAX, y
+    jno @F
+    mov errno, E_RANGE
+ @@:
+    ret
+    
+addition endp
+
+subtraction proc x:DWORD,y:DWORD
+    
+    mov EAX, x
+    sub EAX, y
+    jno @F
+    mov errno, E_RANGE
+ @@:
+    ret
+    
+subtraction endp
+
+multiply proc x:DWORD,y:DWORD
+    
+    mov EAX, x
+    imul EAX, y
+    jno @F
+    mov errno, E_RANGE
+ @@:
+    ret
+    
+multiply endp
+
+division proc x:DWORD,y:DWORD
+    
+    mov EAX, x
+    xor EDX, EDX
+    cdq
+    idiv y
+    jno @F
+    mov errno, E_RANGE
+ @@:
+    ret
+   
+division endp
+
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+
+end start
---
+++ Lab6.asm
@@ -0,0 +1,189 @@
+    .486                       ; create 32 bit code
+    .model flat, stdcall       ; 32 bit memory model
+    option casemap :none       ; case sensitive
+ 
+    __UNICODE__ equ 1
+    
+    E_RANGE      equ  1
+    E_OPNOTSUPP  equ  2
+    E_SYNTAX     equ  3
+ 
+    include windows.inc
+    include masm32.inc
+    include kernel32.inc
+    include shell32.inc
+    include msvcrt.inc
+    include \masm32\macros\macros.asm
+
+    includelib masm32.lib
+    includelib kernel32.lib
+    includelib shell32.lib
+    includelib msvcrt.lib
+
+    apply_operation PROTO :REAL8, :REAL8
+    print_help PROTO
+    print_error PROTO
+    
+; ariphmetic operations
+
+    addition PROTO :REAL8, :REAL8
+    subtraction PROTO :REAL8, :REAL8
+    multiply PROTO :REAL8, :REAL8
+    division PROTO :REAL8, :REAL8
+
+    .data
+    errno       dd   0
+    operation   dd   0
+    result      REAL8   ?
+    
+    .code
+
+start:
+ 
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+
+main proc
+
+    LOCAL args: LPWSTR
+    LOCAL nArgs: DWORD
+    LOCAL ArgList: DWORD
+    LOCAL x: REAL8
+    LOCAL y: REAL8
+    
+    invoke GetCommandLineW
+    mov args, EAX
+    invoke CommandLineToArgvW, args, addr nArgs
+    mov ArgList, EAX
+    .IF ArgList == 0
+        print "Error: can't get command line arguments."
+        jmp close
+    .ENDIF
+    cmp nArgs, 4
+    jne command_line_error 
+    
+    mov ESI, ArgList
+
+    add ESI, 4
+    invoke crt_swscanf, dword ptr [ESI], cfm$("%8lf"), ADDR x
+    cmp EAX, 1    
+    jne command_line_error
+    
+    add ESI, 4
+    invoke crt_swscanf, dword ptr [ESI], cfm$("%c"), ADDR operation
+    cmp EAX, 1
+    jne command_line_error
+    
+    add ESI, 4
+    invoke crt_swscanf, dword ptr [ESI], cfm$("%8lf"), ADDR y
+    cmp EAX, 1
+    jne command_line_error
+    
+    invoke apply_operation, x, y
+
+    .IF errno != 0
+        invoke print_error
+    .ELSEIF
+        printf("%lf %c %lf = %lf", x, operation, y, result)
+    .ENDIF
+    
+    jmp close
+    
+  command_line_error:
+    mov errno, E_SYNTAX
+    invoke print_error
+  close:
+; free memory
+    invoke LocalFree, ArgList
+    invoke ExitProcess,0
+    
+    ret
+
+main endp
+
+
+print_help proc
+
+    print "AVS Labs, 2013"
+    print chr$(13,10,13,10)
+    print "SYNTAX: avs-labs [first number] [operation] [second number]"
+    print chr$(13,10)
+    print "        first number : first number"
+    print chr$(13,10)
+    print "        operation    : operation that be applied to expression"
+    print chr$(13,10)
+    print "        second number: another number."
+    print chr$(13,10,13,10)
+    print "        Output is to STDOUT. It can redirected to a file."
+    print chr$(13,10)
+	ret
+	
+print_help endp
+
+
+print_error proc
+
+    .IF errno == E_RANGE
+        print "Error: out of range."
+    .ELSEIF errno == E_OPNOTSUPP
+        print "Error: operation not supported."
+    .ELSEIF errno == E_SYNTAX
+        invoke print_help
+    .ENDIF
+	ret
+	
+print_error endp
+
+apply_operation proc x:REAL8, y:REAL8 
+    
+    .IF operation == "+"
+        invoke addition, x, y
+    .ELSEIF operation == "-"
+        invoke subtraction, x, y
+    .ELSEIF operation == "*"
+        invoke multiply, x, y
+    .ELSEIF operation == "/"
+        invoke division, x, y
+    .ELSEIF
+        mov errno, E_OPNOTSUPP
+    .ENDIF
+	ret
+	
+apply_operation endp
+
+addition proc x:REAL8,y:REAL8
+    
+    fld x
+    fld y
+    fadd
+    fstp result
+    ret
+    
+addition endp
+
+subtraction proc x:REAL8,y:REAL8
+    
+    fld x
+    fld y
+    fsub
+    fstp result
+    ret
+    
+subtraction endp
+
+multiply proc x:REAL8,y:REAL8
+    
+    mov errno, E_RANGE
+    ret
+    
+multiply endp
+
+division proc x:REAL8,y:REAL8
+    
+    mov errno, E_RANGE
+    ret
+   
+division endp
+
+; ллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл
+
+end start
